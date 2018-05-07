a05b5ea8bd953a1748683424379d1e0abddecff9
Dominik Wittenberg <dowi@live.de>
2018-05-07 12:45:39 +0200

    Important functions in separate classes.

---
+++ iTunesManipulation/Helper.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAPICodePack.Shell;
+using iTunesLib;
+using System.Threading;
+
+namespace iTunesManipulation
+{
+    public class Helper
+    {
+        public static string[] compitableMusicTypes = { ".mp3", ".m4a", ".acc" };
+        public static double status = 0;
+
+        /// <summary>
+        /// Load FIleInfos from XML - without Rating
+        /// </summary>
+        /// <returns></returns>
+        
+
+        public static async Task<List<SongStruct>> GetFileRating(List<SongStruct> givenList, CancellationToken ctx, IProgress<double> progress)
+        {
+            List<SongStruct> listOfDifferences = new List<SongStruct>();
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < givenList.Count; i++)
+                {
+                    if (ctx.IsCancellationRequested)
+                        break;
+
+                    givenList[i].RatingFile = RatingReadFile(givenList[i].Location);
+                    Report(progress, i, givenList.Count);
+                }
+            }, ctx);
+
+            return listOfDifferences;
+        }
+
+        public static async Task<List<SongStruct>> GetItunesRating(List<SongStruct> givenList, iTunesAppClass _myiTunes, CancellationToken ctx, IProgress<double> progress)
+        {
+            List<SongStruct> listOfDifferences = new List<SongStruct>();
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < givenList.Count; i++)
+                {
+                    if (ctx.IsCancellationRequested)
+                        break;
+
+                    // ToDo: Testen mit kleiner Bibliotek
+                    var a = _myiTunes.LibraryPlaylist.Tracks.ItemByName[givenList[i].Name] as IITTrack;
+                    givenList[i].RatingiTunes = a.Rating.ItunesRatingTo5();
+                    Report(progress, i, givenList.Count);
+                }
+            }, ctx);
+
+            return givenList;
+        }
+
+        public static async Task<List<SongStruct>> FilterDifferenzes(List<SongStruct> givenList, CancellationToken ctx, IProgress<double> progress)
+        {
+            List<SongStruct> listOfDifferences = new List<SongStruct>();
+            //ConcurrentBag<SongStruct> bagOfDifferences = new ConcurrentBag<SongStruct>();
+
+            await Task.Run(() =>
+            {
+                //givenList.AsParallel().ForAll(song =>
+                //{
+                //    if (ctx.IsCancellationRequested)
+                //        return;
+
+                //    if ((song.RatingiTunes == 0 &&
+                //        song.RatingFile != 0) |
+                //        song.RatingFile != song.RatingiTunes)
+                //    {
+                //        bagOfDifferences.Add(song);
+                //    }
+
+                //    progress.Report(1);
+                //});
+                for (int i = 0; i < givenList.Count; i++)
+                {
+
+                    if (ctx.IsCancellationRequested)
+                        break;
+
+                    if ((givenList[i].RatingiTunes == 0 &&
+                        givenList[i].RatingFile != 0) |
+                        givenList[i].RatingFile != givenList[i].RatingiTunes)
+                    {
+                        listOfDifferences.Add(givenList[i]);
+                    }
+
+                    Report(progress, i, givenList.Count);
+                }
+            }, ctx);
+
+            return listOfDifferences;
+        }
+
+        #region read/write file rating
+        public static int RatingReadFile(string location)
+        {
+            ShellFile so = ShellFile.FromFilePath(location);
+
+            if (!compitableMusicTypes.Contains<string>(location.Split('.').Last()) | so.Properties.System.Rating.Value == null)
+            { return 0; }
+
+            return so.Properties.System.Rating.Value.FileRatingTo5();
+        }
+
+        public static async Task<bool> Test(CancellationToken ctx, IProgress<double> progress)
+        {
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (ctx.IsCancellationRequested)
+                    break;
+                await Task.Delay(25);
+                Report(progress, i, 100);
+            }
+
+            return true;
+        }
+
+
+        public static bool RatingWrite(FileInfo file, int rating)
+        {
+            try
+            {
+                ShellFile so = ShellFile.FromFilePath(file.FullName);
+
+                if (!compitableMusicTypes.Contains<string>(file.Extension))
+                    return false;
+
+                so.Properties.System.Rating.Value = rating.ToFileRating();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        private static void Report(IProgress<double> progress, int value, int max)
+        {
+            progress.Report((value * 100.0) / max);
+        }
+        #endregion
+    }
+}
---
+++ iTunesManipulation/XmlHandler.cs
@@ -0,0 +1,105 @@
+﻿using Claunia.PropertyList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace iTunesManipulation
+{
+    static class XmlHandler
+    {
+        public static List<SongStruct> LoadItunesXML(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("invalid filepath: '" + filePath + "'");
+                return new List<SongStruct>();
+            }
+
+            List<SongStruct> bibliotek = new List<SongStruct>();
+            NSDictionary tracks = (XmlPropertyListParser.Parse(new FileInfo(filePath)) as NSDictionary)["Tracks"] as NSDictionary;
+
+            foreach (var entry in tracks)
+            {
+                NSDictionary track = (NSDictionary)entry.Value;
+                SongStruct musicFile = new SongStruct();
+
+                // Get importend informations
+                if (track.ContainsKey("Location"))
+                { musicFile.Location = Path2String(track["Location"].ToString()); }
+
+                // ToDo: set Filter?
+                // if (File.Exists(musicFile.Location))
+                //    continue;
+
+                // strings
+                if (track.ContainsKey("Name"))
+                { musicFile.Name = track["Name"].ToString(); }
+
+                if (track.ContainsKey("Album"))
+                { musicFile.Album = track["Album"].ToString(); }
+
+                if (track.ContainsKey("Artist"))
+                { musicFile.Album = track["Artist"].ToString(); }
+
+                if (track.ContainsKey("AlbumA rtist"))
+                { musicFile.Album = track["Album Artist"].ToString(); }
+
+                if (track.ContainsKey("Genre"))
+                { musicFile.Genre = track["Genre"].ToString(); }
+
+                // integer
+                if (track.ContainsKey("Track Number"))
+                { musicFile.Track = int.Parse(track["Track Number"].ToString()); }
+
+                if (track.ContainsKey("Year"))
+                { musicFile.Year = int.Parse(track["Year"].ToString()); }
+
+                if (track.ContainsKey("Album Rating"))
+                { musicFile.Year = int.Parse(track["Album Rating"].ToString()); }
+
+                if (track.ContainsKey("Total Time"))
+                { musicFile.Year = int.Parse(track["Total Time"].ToString()); }
+
+                if (track.ContainsKey("Bit Rate"))
+                { musicFile.Year = int.Parse(track["Bit Rate"].ToString()); }
+
+                if (track.ContainsKey("Play Count"))
+                { musicFile.Year = int.Parse(track["Play Count"].ToString()); }
+
+                //// bool
+                //if (track.ContainsKey("Rating Computed"))
+                //{ musicFile.RatingComputed = (track["Rating Computed"].ToString() == "true") ? true : false; }
+
+                // ToDo: TryCatch ??
+
+                bibliotek.Add(musicFile);
+            }
+            return bibliotek;
+        }
+
+        public static string Path2String(string pathStr)
+        {
+            Uri pathUri = new Uri(pathStr);
+            string path = pathUri.LocalPath;
+            path = path.Replace("string: ", "");
+            path = path.Replace("\\\\localhost\\", string.Empty);
+            path = path.Replace(" file://localhost/", string.Empty);
+            path = path.Replace("file://localhost/", string.Empty);
+
+            return path;
+        }
+
+        public static string String2Path(string path)
+        {
+            path = path.Replace("string: ", "");
+            path = path.Replace("file://localhost/", string.Empty);
+
+            Uri pathUri = new Uri(path);
+            string uri = pathUri.AbsoluteUri;
+            uri = "string: " + uri.Replace("file:///", "file://localhost/");
+
+            return uri;
+        }
+    }
+}
